0c98397e4bbf16fc397e37cc05e3fe7d68eb1f19
Vadim <vrastvorov@fil-it.ru>
2023-01-17 13:13:10 +0600

    09.01.2023 Добавил Тесты Контролеров

--- src/test/java/ru/onbording/employeeservice/controller/EmployeeControllerTest.java
+++ src/test/java/ru/onbording/employeeservice/controller/EmployeeControllerTest.java
@@ -1,6 +1,54 @@
 package ru.onbording.employeeservice.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 import ru.onbording.employeeservice.DatabaseTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
 public class EmployeeControllerTest extends DatabaseTest {
+    private static final String URL = "/api/employee";
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void testFetchEmployeeById() throws Exception {
+        mvc.perform(get(URL + "/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testDeleteEmployee() throws Exception {
+        mvc.perform(delete(URL + "/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testAllEmployee() throws Exception {
+        mvc.perform(get(URL + "/all"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testCreateEmployee() throws Exception {
+        mvc.perform(post(URL + ""))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testCreateListEmployee() throws Exception {
+        mvc.perform(post(URL + "/list"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testUpdateEmployee() throws Exception {
+        mvc.perform(put(URL + ""))
+                .andExpect(status().isOk());
+    }
 }
--- src/test/java/ru/onbording/employeeservice/controller/TaskControllerTest.java
+++ src/test/java/ru/onbording/employeeservice/controller/TaskControllerTest.java
@@ -1,6 +1,56 @@
 package ru.onbording.employeeservice.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 import ru.onbording.employeeservice.DatabaseTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
 public class TaskControllerTest extends DatabaseTest {
+    private static final String URL = "/api/task";
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void testFetchTaskById() throws Exception {
+        mvc.perform(get(URL + "/d4189f5f-2ec9-4097-98c9-f6186c26b5da"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testDeleteTask() throws Exception {
+        mvc.perform(delete(URL + "/d4189f5f-2ec9-4097-98c9-f6186c26b5da"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testFetchAllTask() throws Exception {
+        mvc.perform(get(URL + "/all"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testFetchTaskByEmployeeId() throws Exception {
+        mvc.perform(get(URL + "/employee/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testCreateTask() throws Exception {
+        mvc.perform(post(URL + ""))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testUpdateTask() throws Exception {
+        mvc.perform(put(URL + ""))
+                .andExpect(status().isOk());
+    }
 }
